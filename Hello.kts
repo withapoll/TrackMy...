dff8e7d3a1a953250eb41d2ec685489e69a4509b
Andrew <withapoll@outlook.com>
2024-06-12 15:32:24 +0200

    Update Hello.kts

--- Hello.kts
+++ Hello.kts
@@ -1,5 +1,5 @@
 fun main() {
     println("This app is in development")
     //and there will be no source code
-    //bc this app will be published in Google Store soon...
+    //bc this app will be published in the Google Play Store soon...
 }
